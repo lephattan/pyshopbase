2f0d1acaa9b301ff4410130549aa590397e7b6c9
Tan le <tanle2207@gmail.com>
2019-06-30 23:48:23 +0700

    Add setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyshopbase',
-    version='0.0.1',
+    version='0.0.2',
     author='Tan Le',
     author_email='tanle2207@gmail.com',
     description="Python Wrapper for Shopbase Api",
