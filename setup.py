0f2045f3cc3bb26ae9d8709122bbf2b4e6d0d3cd
Tan le <tanle2207@gmail.com>
2019-06-30 22:22:33 +0700

    First Commit with Some Code

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyshopbase-lephattan',
+    version='0.0.1',
+    author='Tan Le',
+    author_email='tanle2207@gmail.com',
+    description="Python Wrapper for Shopbase Api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lephattan/pyshopbase',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ]
+)
