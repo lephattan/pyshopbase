6ccdf1f1c3db38c266525693121e8075895aa6e0
lephattan <tanle2207@gmail.com>
2019-07-11 15:37:12 +0700

    Optimze ApiResponse Code

--- pyshopbase/response.py
+++ pyshopbase/response.py
@@ -3,16 +3,13 @@ class ApiResponse:
         self.raw_respone = r
         self.json = {}
         self.status = ''
-        self.status_code = None
+        self.status_code = r.status_code
         if r.status_code == 200:
             self.status = 'success'
-            self.status_code = r.status_code
             self.json = r.json()
         elif r.status_code == 503:
             self.status_code = 'fail'
-            self.status_code = r.status_code
             self.json = {'message': r.content}
         else:
             self.status_code = 'fail'
-            self.status_code = r.status_code
             self.json = {'message': r.content}
